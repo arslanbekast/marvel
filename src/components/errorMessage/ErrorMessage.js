d3d0d9bc1fe0ecf175425b5f2247798a185fed3b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-02-26 20:47:45 +0300

    Implemented beautiful output of the RandomChar component

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,15 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img style={{
+            display: 'block',
+            width: "250px",
+            heght: "250px",
+            objectFit: 'contain',
+            margin: "0 auto"
+        }} src={img} alt="Error"/>
+    )
+}
+
+export default ErrorMessage;
