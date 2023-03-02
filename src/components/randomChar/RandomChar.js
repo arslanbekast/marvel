2b647f965711aff2d72e192500695762f92aed5b
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-03-02 18:01:26 +0300

    Implemented the first part of the functionality of the CharList component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -33,6 +33,12 @@ class RandomChar extends Component {
         });
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        });
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -41,7 +47,8 @@ class RandomChar extends Component {
     }
 
     updateChar = () => {
-        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);;
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,16 +4,16 @@ class MarvelService {
 
     getResource = async (url) => {
         let res = await fetch(url);
-
+    
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-
+    
         return await res.json();
     }
 
     getAllCharacters = async () => {
-        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -24,13 +24,14 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
         }
-    }   
+    }  
 }
 
 export default MarvelService;
