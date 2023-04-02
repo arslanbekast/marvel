19c81ee6066093e6ebfcb58a075b38c7ad909f39
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-04-02 16:27:26 +0300

    The functionality of the SingleComicPage component has been implemented

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,7 +25,7 @@ const useMarvelService = () => {
 		return res.data.results.map(_transformComics);
 	};
 
-	const getComics = async (id) => {
+	const getComic = async (id) => {
 		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
 		return _transformComics(res.data.results[0]);
 	};
@@ -68,7 +68,7 @@ const useMarvelService = () => {
 		getAllCharacters,
 		getCharacter,
 		getAllComics,
-		getComics,
+		getComic,
 	};
 }
 
