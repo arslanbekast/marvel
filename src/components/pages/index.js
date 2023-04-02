53668630e1bbfd7e0d98fe0c0b53c5008df7b04e
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-04-02 11:03:46 +0300

    Implemented routing in the project

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+import MainPage from "./MainPage";
+import ComicsPage from "./ComicsPages";
+
+export {MainPage, ComicsPage};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
 
+// ReactDOM
+//   .createRoot(document.getElementById('root'))
+//   .render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   );
 
