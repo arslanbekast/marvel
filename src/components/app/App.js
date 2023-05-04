80f62ea39e0da58222e40a3dbb665cd2a4fd01e9
Arslanbek Astemirov <astemirow@mail.ru>
2023-05-04 20:45:39 +0300

    Functionality of CharSearchForm component implemented

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,9 @@ import Spinner from "../spinner/Spinner";
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 const App = () => {
 
@@ -21,7 +23,8 @@ const App = () => {
                         <Routes>
                             <Route path="/" element={<MainPage/>} />
                             <Route path="/comics" element={<ComicsPage/>} />
-                            <Route path="/comics/:comicId" element={<SingleComicPage/>} />
+                            <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
+                            <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
                             <Route path="*" element={<Page404/>} />
                         </Routes>
                     </Suspense>
