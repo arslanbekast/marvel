b8d49b0c6f7c5a5bb3583fdf6b14362fbdb3c577
Arslanbek Astemirov <astemirow@mail.ru>
2023-05-08 11:11:39 +0300

    The project was SEO-optimized using the react-helmet library

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,19 @@
+import { Helmet } from "react-helmet";
+
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comics page </title>
+            </Helmet>
+            
             <AppBanner/>
             <ComicsList/>
         </>
--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,4 +1,5 @@
 import './singleCharacterLayout.scss';
+import { Helmet } from "react-helmet";
 
 const SingleCharacterLayout = ({data}) => {
 
@@ -6,6 +7,13 @@ const SingleCharacterLayout = ({data}) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${name} page`}
+                />
+                <title>{name}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
